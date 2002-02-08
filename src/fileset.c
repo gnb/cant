916838debfaf4260b2e5b2d4170ba148293c0413
Greg Banks <gnb@fmeh.org>
2002-02-08 07:29:25 +0000

    x  Completely rewrote fileset implementation to do pattern-driven    globbing instead of pattern matching against filename recursion.    Recursion now begins only if the "**" operator is used.  Also,    semantics of <fileset dir=""> are now clearer. x  Filesets are refcounted to avoid problems when deleting projects. x  fileset_apply() and fileset_gather_mapped() now return void.

--- src/fileset.c
+++ src/fileset.c
@@ -19,144 +19,14 @@
 
 #include "fileset.h"
 #include "estring.h"
+#include "tok.h"
 #include "mapper.h"
 #include "log.h"
 #include <dirent.h>
 
-CVSID("$Id: fileset.c,v 1.11 2002-02-04 05:13:16 gnb Exp $");
+CVSID("$Id: fileset.c,v 1.12 2002-02-08 07:29:25 gnb Exp $");
 
-typedef enum { FS_IN, FS_EX, FS_UNKNOWN } fs_result_t;
-
-/*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
-
-static fs_spec_t *
-fs_spec_new(
-    unsigned flags,
-    const char *pattern,
-    gboolean case_sens,
-    const char *filename)
-{
-    fs_spec_t *fss;
-    
-    fss = new(fs_spec_t);
-    
-    fss->flags = flags;
-    
-    if (pattern != 0)
-    	pattern_init(&fss->pattern, pattern, (case_sens ? PAT_CASE : 0));
-    
-    strassign(fss->filename, filename);
-    condition_init(&fss->condition);
-    
-    return fss;
-}
-
-static void
-fs_spec_delete(fs_spec_t *fss)
-{
-    strdelete(fss->filename);
-    pattern_free(&fss->pattern);
-    condition_free(&fss->condition);
-
-    g_free(fss);
-}
-
-/*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
-
-static fs_result_t fs_spec_match(const fileset_t *, const props_t *, fs_spec_t *, const char *);
-
-static fs_result_t
-fs_spec_list_match(
-    const fileset_t *fs,
-    const props_t *props,
-    GList *list,
-    const char *filename)
-{
-    fs_result_t res = FS_UNKNOWN;
-        
-    for ( ; list != 0 ; list = list->next)
-    {
-    	fs_spec_t *fss = (fs_spec_t *)list->data;
-	
-	switch (fs_spec_match(fs, props, fss, filename))
-	{
-	case FS_IN:
-	    res = FS_IN;
-	    break;
-	case FS_EX:
-	    res = FS_EX;
-	    break;
-	case FS_UNKNOWN:
-	    break;
-	}
-    }
-
-    return res;
-}
-
-/* returns a list of fs_spec_t* */
-static GList *
-fs_spec_file_read(const fileset_t *fs, fs_spec_t *fss)
-{
-    fs_spec_t *child;
-    FILE *fp;
-    GList *list = 0;
-    char *x, buf[1024];
-        
-    /* read the file */
-    if ((fp = fopen(fss->filename, "r")) == 0)
-    {
-    	log_perror(fss->filename);
-	return 0;
-    }
-    
-    while (fgets(buf, sizeof(buf), fp) != 0)
-    {
-    	if ((x = strchr(buf, '\n')) != 0)
-	    *x = '\0';
-    	if ((x = strchr(buf, '\r')) != 0)
-	    *x = '\0';
-
-    	child = fs_spec_new(fss->flags & FS_INCLUDE,
-	    	    	    /*pattern*/buf, fs->case_sensitive,
-			    /*filename*/0);
-	list = g_list_prepend(list, child);
-    }
-    
-    fclose(fp);
-    
-    return g_list_reverse(list);
-}
-
-
-static fs_result_t
-fs_spec_match(
-    const fileset_t *fs,
-    const props_t *props,
-    fs_spec_t *fss,
-    const char *filename)
-{
-    if (!condition_evaluate(&fss->condition, props))
-	    return FS_UNKNOWN;
-    
-    if (fss->flags & FS_FILE)
-    {
-    	GList *list;
-	fs_result_t res;
-	
-	/* TODO: caching should be global on filename with mod time checking */
-	list = fs_spec_file_read(fs, fss);
-    	res = fs_spec_list_match(fs, props, list, filename);
-    	listdelete(list, fs_spec_t, fs_spec_delete);
-	
-	return res;
-    }
-    
-    if (!pattern_match(&fss->pattern, filename))
-    	return FS_UNKNOWN;
-	
-    return ((fss->flags & FS_INCLUDE) ? FS_IN : FS_EX);
-}
+static void fs_spec_delete(fs_spec_t *fss);
 
 /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
 
@@ -166,27 +36,36 @@ fileset_new(void)
     fileset_t *fs;
     
     fs = new(fileset_t);
+    fs->refcount = 1;
 
     return fs;
 }
 
 /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
 
-void
+static void
 fileset_delete(fileset_t *fs)
 {
-    while (fs->specs != 0)
-    {
-    	fs_spec_t *fss = (fs_spec_t *)fs->specs->data;
-	
-	fs_spec_delete(fss);
-	fs->specs = g_list_remove_link(fs->specs, fs->specs);
-    }
-
+    listdelete(fs->specs, fs_spec_t, fs_spec_delete);
     strdelete(fs->id);
     strdelete(fs->directory);
 	
     g_free(fs);
+}
+
+/*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
+
+void
+fileset_ref(fileset_t *fs)
+{
+    fs->refcount++;
+}
+
+void
+fileset_unref(fileset_t *fs)
+{
+    if (--fs->refcount == 0)
+    	fileset_delete(fs);
 }
 
 /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
@@ -205,48 +84,63 @@ fileset_set_directory(fileset_t *fs, const char *dir)
 
 /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
 
-fs_spec_t *
-fileset_add_include(fileset_t *fs, const char *pattern)
+static fs_spec_t *
+fs_spec_add(
+    fileset_t *fs,
+    unsigned flags,
+    const char *pattern,
+    const char *filename)
 {
     fs_spec_t *fss;
     
-    fss = fs_spec_new(FS_INCLUDE, pattern, fs->case_sensitive, /*filename*/0);
-    fs->specs = g_list_append(fs->specs, fss);
+    fss = new(fs_spec_t);
     
+    fss->flags = flags;
+    
+    if (pattern != 0)
+    	pattern_init(&fss->pattern, pattern,
+	    	    	(fs->case_sensitive ? PAT_CASE : 0));
+    
+    strassign(fss->filename, filename);
+    condition_init(&fss->condition);
+
+    fs->specs = g_list_append(fs->specs, fss);
+        
     return fss;
+}
+
+static void
+fs_spec_delete(fs_spec_t *fss)
+{
+    strdelete(fss->filename);
+    pattern_free(&fss->pattern);
+    condition_free(&fss->condition);
+
+    g_free(fss);
+}
+
+fs_spec_t *
+fileset_add_include(fileset_t *fs, const char *pattern)
+{
+    return fs_spec_add(fs, FS_INCLUDE, /*pattern*/0, pattern);
 }
 
 fs_spec_t *
 fileset_add_include_file(fileset_t *fs, const char *filename)
 {
-    fs_spec_t *fss;
-    
-    fss = fs_spec_new(FS_INCLUDE|FS_FILE, /*pattern*/0, fs->case_sensitive, filename);
-    fs->specs = g_list_append(fs->specs, fss);
-    
-    return fss;
+    return fs_spec_add(fs, FS_INCLUDE|FS_FILE, /*pattern*/0, filename);
 }
 
 fs_spec_t *
 fileset_add_exclude(fileset_t *fs, const char *pattern)
 {
-    fs_spec_t *fss;
-    
-    fss = fs_spec_new(0, pattern, fs->case_sensitive, /*filename*/0);
-    fs->specs = g_list_append(fs->specs, fss);
-    
-    return fss;
+    return fs_spec_add(fs, FS_EXCLUDE, pattern, /*filename*/0);
 }
 
 fs_spec_t *
 fileset_add_exclude_file(fileset_t *fs, const char *filename)
 {
-    fs_spec_t *fss;
-    
-    fss = fs_spec_new(FS_FILE, /*pattern*/0, fs->case_sensitive, filename);
-    fs->specs = g_list_append(fs->specs, fss);
-    
-    return fss;
+    return fs_spec_add(fs, FS_EXCLUDE|FS_FILE, /*pattern*/0, filename);
 }
 
 /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
@@ -265,79 +159,327 @@ fileset_set_case_sensitive(fileset_t *fs, gboolean b)
 
 /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
 
-static int
-fileset_apply_1(
-    const fileset_t *fs,
-    const props_t *props,
-    const char *filename,
-    file_apply_proc_t function,
-    void *userdata)
+static void
+list_splice(GList *after, GList *list2)
+{
+    GList *last2;
+    
+    if (list2 == 0)
+    	return;
+	
+    last2 = g_list_last(list2);
+
+    if (after->next != 0)
+	after->next->prev = last2;
+    last2->next = after->next;
+    
+    after->next = list2;
+    list2->prev = after;
+}
+
+typedef struct
+{
+    char *basedir;
+    GList *filenames;
+    GList *pending;
+    gboolean case_sens;
+    gboolean check_exists;
+} fs_glob_state_t;
+
+/*
+ * Replace the single list item `baselink' with a list of
+ * files matching the glob-directory-component `globpart'.
+ */
+static void
+glob_part(
+    fs_glob_state_t *state,
+    const char *globpart,
+    GList *baselink)
+{
+    pattern_t pat;
+    DIR *dir;
+    struct dirent *de;
+    GList *results = 0;
+    char *base = (char *)baselink->data;
+    char *newpath;
+
+    if ((dir = file_opendir(*base == '\0' ? "." : base)) != 0)
+    {
+	pattern_init(&pat, globpart, (state->case_sens ? PAT_CASE : 0));
+
+	while ((de = readdir(dir)) != 0)
+	{
+    	    if (!strcmp(de->d_name, ".") ||
+		!strcmp(de->d_name, ".."))
+		continue;
+
+    	    if (pattern_match_c(&pat, de->d_name))
+	    {
+		newpath = (*base == '\0' ?
+			    g_strdup(de->d_name) :
+	    	    	    g_strconcat(base, "/", de->d_name, 0));
+#if DEBUG
+    		fprintf(stderr, "glob_part(\"%s\") -> \"%s\"\n", base, newpath);
+#endif
+		results = g_list_append(results, newpath);
+	    }
+	}
+
+	closedir(dir);
+	pattern_free(&pat);
+    }
+
+
+    list_splice(baselink, results);
+    g_free(base);
+    state->pending = g_list_remove_link(state->pending, baselink);
+}
+
+
+static void
+glob_recurse(
+    fs_glob_state_t *state,
+    const pattern_t *pat,
+    const char *base)
 {
     DIR *dir;
     struct dirent *de;
-    estring child;
-    int ret = 1;
-    
-    if ((dir = file_opendir(filename)) == 0)
-    	return -1;
-	
-    estring_init(&child);
-    while ((de = readdir(dir)) != 0)
+    char *newpath;
+
+    if ((dir = file_opendir(*base == '\0' ? "." : base)) != 0)
     {
-    	if (!strcmp(de->d_name, ".") ||
-	    !strcmp(de->d_name, ".."))
-	    continue;
-	    
-	/* TODO: estring_truncate_to() */
-	estring_truncate(&child);
-	if (strcmp(filename, "."))
+	while ((de = readdir(dir)) != 0)
 	{
-	    estring_append_string(&child, filename);
-	    estring_append_char(&child, '/');
+    	    if (!strcmp(de->d_name, ".") ||
+		!strcmp(de->d_name, ".."))
+		continue;
+
+	    newpath = (*base == '\0' ?
+			g_strdup(de->d_name) :
+	    	    	g_strconcat(base, "/", de->d_name, 0));
+
+	    if (file_is_directory(newpath) == 0)
+	    {
+	    	glob_recurse(state, pat, newpath);
+		g_free(newpath);
+	    }
+    	    else if (pattern_match_c(pat, newpath))
+	    {
+#if DEBUG
+    		fprintf(stderr, "glob_recurse(\"%s\") -> \"%s\"\n",
+		    	    	pat->pattern, newpath);
+#endif
+    	    	state->pending = g_list_append(state->pending, newpath);
+	    }
+	    else
+	    	g_free(newpath);
 	}
-	estring_append_string(&child, de->d_name);
-	
-	if (file_is_directory(child.data) == 0)
+
+	closedir(dir);
+    }
+}
+
+/*
+ * Replace the single list item `baselink' with a list of
+ * files matching the glob-directory-sequence `globseq'.
+ */
+static void
+glob_path(
+    fs_glob_state_t *state,
+    char *globpath,
+    GList *baselink)
+{
+    const char *part;
+    GList *iter, *next;
+    tok_t tok;
+    
+    tok_init(&tok, globpath, "/");
+
+    while ((part = tok_next(&tok)) != 0)
+    {
+	if (!strcmp(part, "**"))
 	{
-	    if ((ret = fileset_apply_1(fs, props, child.data, function, userdata)) != 1)
-	    	break;
+	    pattern_t pat;
+	    GList *oldpending;
+	    
+    	    pattern_init(&pat, globpath, (state->case_sens ? PAT_CASE : 0));
+
+    	    oldpending = state->pending;
+	    state->pending = 0;
+	    while (oldpending != 0)
+	    {
+	    	char *base = (char *)oldpending->data;
+		glob_recurse(state, &pat, base);
+		g_free(base);
+	    	oldpending = g_list_remove_link(oldpending, oldpending);
+	    }
+	    break;
+	}
+	else if (strpbrk(part, "*?[]"))
+	{
+	    state->check_exists = TRUE;
+	    for (iter = state->pending ; iter != 0 ; iter = next)
+	    {
+	    	next = iter->next;
+		glob_part(state, part, iter);
+	    }
 	}
 	else
 	{
-	    /* files */
-    	    if (fs_spec_list_match(fs, props, fs->specs, child.data) != FS_IN)
-		continue;
-
-	    if (!(*function)(child.data, userdata))
+	    for (iter = state->pending ; iter != 0 ; iter = next)
 	    {
-		ret = 0;
-		break;
+	    	char *fn = (char *)iter->data;
+	    	next = iter->next;
+		
+	    	fn = g_realloc(fn, strlen(fn)+1+strlen(part)+1);
+		if (*fn != '\0')
+		    strcat(fn, "/");
+		strcat(fn, part);
+		iter->data = fn;
+		
+		if (state->check_exists && file_exists(fn) < 0)
+		{
+		    g_free(fn);
+		    state->pending = g_list_remove_link(state->pending, iter);
+		}
 	    }
-    	}
+	}
     }
-    
-    estring_free(&child);
-    closedir(dir);
-    return ret;
+
+    tok_free(&tok);
+    g_free(globpath);
 }
 
-int
+static void
+fs_include(
+    fs_glob_state_t *state,
+    char *glob)
+{
+    state->pending = g_list_append(0, g_strdup(*glob == '/' ? "/" : ""));
+    state->check_exists = FALSE;
+    
+    glob_path(state, glob, state->pending);
+
+    if (((char *)state->pending->data)[0] == '\0')
+    {
+#if DEBUG
+    	fprintf(stderr, "fs_include(\"%s\") matched no files\n", glob);
+#endif
+    	listdelete(state->pending, char, g_free);
+    }
+    else
+    {
+#if DEBUG
+    	GList *iter;
+    	fprintf(stderr, "fs_include(\"%s\") adding", glob);
+	for (iter = state->pending ; iter != 0 ; iter = iter->next)
+	    fprintf(stderr, " \"%s\"", (char *)iter->data);
+	fprintf(stderr, "\n");
+#endif
+	state->filenames = g_list_concat(state->filenames, state->pending);
+    }
+}
+
+
+static void
+fs_exclude(
+    fs_glob_state_t *state,
+    pattern_t *pat)
+{
+    GList *iter, *next;
+    
+    for (iter = state->filenames ; iter != 0 ; iter = next)
+    {
+    	char *fn = (char *)iter->data;
+    	next = iter->next;
+	
+	if (pattern_match_c(pat, fn))
+	{
+#if DEBUG
+    	    fprintf(stderr, "fs_exclude(\"%s\"): removing \"%s\"\n",
+	    	    	pat->pattern, fn);
+#endif
+	    g_free(fn);
+	    state->filenames = g_list_remove_link(state->filenames, iter);
+	}
+    }
+}
+
+static void
+fs_apply_file(
+    fs_glob_state_t *state,
+    const char *pattfile,
+    gboolean include)
+{
+    FILE *fp;
+    char *x, buf[1024];
+        
+    /* read the file */
+    if ((fp = fopen(pattfile, "r")) == 0)
+    {
+    	log_perror(pattfile);
+	return;
+    }
+    
+    while (fgets(buf, sizeof(buf), fp) != 0)
+    {
+    	if ((x = strchr(buf, '\n')) != 0)
+	    *x = '\0';
+    	if ((x = strchr(buf, '\r')) != 0)
+	    *x = '\0';
+
+    	if (include)
+	    fs_include(state, file_normalise_m(buf, state->basedir));
+	else
+	{
+	    pattern_t pat;
+	    
+	    pattern_init(&pat, buf, (state->case_sens ? PAT_CASE : 0));
+	    fs_exclude(state, &pat);
+	    pattern_free(&pat);
+	}
+    }
+    
+    fclose(fp);
+}
+
+
+void
 fileset_apply(
     const fileset_t *fs,
     const props_t *props,
     file_apply_proc_t func,
     void *userdata)
 {
-    char *expdir;
-    int ret;
+    GList *iter;
+    fs_glob_state_t state;
 
-    expdir = props_expand(props, fs->directory);
+    state.basedir = props_expand(props, fs->directory);
+    state.filenames = 0;
+    state.pending = 0;    
+    state.case_sens = fs->case_sensitive;
+    
+    for (iter = fs->specs ; iter != 0 ; iter = iter->next)
+    {
+    	fs_spec_t *fss = (fs_spec_t *)iter->data;
+	
+	if (fss->flags & FS_FILE)
+	    fs_apply_file(&state, fss->filename, (fss->flags & FS_INCLUDE));
+	else if (fss->flags & FS_INCLUDE)
+	    fs_include(&state, file_normalise(fss->filename, state.basedir));
+	else
+	    fs_exclude(&state, &fss->pattern);
+    }
+    
+    g_free(state.basedir);
+    
+    for (iter = state.filenames ; iter != 0 ; iter = iter->next)
+    {
+	if (!(*func)((char*)iter->data, userdata))
+	    break;
+    }
 
-    ret = fileset_apply_1(fs, props, expdir, func, userdata);
-    
-    g_free(expdir);
-    
-    return ret;
+    listdelete(state.filenames, char, g_free);
 }
 
 /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
@@ -377,7 +519,7 @@ fileset_gather_one(const char *filename, void *userdata)
     return TRUE;   /* keep going */
 }
 
-int
+void
 fileset_gather_mapped(
     const fileset_t *fs,
     const props_t *props,
@@ -389,7 +531,7 @@ fileset_gather_mapped(
     rec.mappers = mappers;
     rec.strarray = sa;
     
-    return fileset_apply(fs, props, fileset_gather_one, &rec);
+    fileset_apply(fs, props, fileset_gather_one, &rec);
 }
 
 /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
